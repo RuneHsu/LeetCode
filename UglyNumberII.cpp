3979bd5c5397d07955cbcfe022697bedb7390f89
RuneHsu <RuneHsu@users.noreply.github.com>
2016-03-28 12:40:56 +0200

    Update UglyNumberII.cpp

--- UglyNumberII.cpp
+++ UglyNumberII.cpp
@@ -84,3 +84,33 @@ public:
     return v.back();
     }
 };
+
+// general solution for different prime factor
+  class Solution {  
+  public:  
+    int nthUglyNumber(int n) {  
+      vector<int> factors{ 2, 3, 5};  
+      return nthUglyNumberGeneral(n, factors);  
+    }  
+    int nthUglyNumberGeneral(int n, vector<int>& factors) {  
+      vector<int> uglys(1,1);  
+      vector<int> indexes(factors.size(), 0);  
+      while(uglys.size() < n) {  
+        int min_v = INT_MAX;  
+        int min_index = 0;  
+        for(int k =0; k< factors.size(); k++) {  
+          int temp = uglys[indexes[k]] * factors[k];  
+          if(temp < min_v) {  
+            min_v = temp;  
+            min_index = k;  
+          }  
+        }  
+        indexes[min_index]++;  
+        // need to avoid duplicate ugly number  
+        if(uglys[uglys.size()-1] != min_v) {  
+          uglys.push_back(min_v);  
+         }  
+      }  
+      return uglys[n-1];  
+    }  
+  };  
