3ab0c479963c3be660e3cfce57eb03f299efb3a4
RuneHsu <RuneHsu@users.noreply.github.com>
2016-04-03 23:50:45 +0200

    Create SymmetricTree.cpp

---
+++ SymmetricTree.cpp
@@ -0,0 +1,67 @@
+// Source: https://leetcode.com/problems/symmetric-tree/
+// Date: 2016-04-03
+/********************************************************************************** 
+* 
+* Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+* 
+* For example, this binary tree is symmetric:
+* 
+*     1
+*    / \
+*   2   2
+*  / \ / \
+* 3  4 4  3
+* 
+* But the following is not:
+* 
+*     1
+*    / \
+*   2   2
+*    \   \
+*    3    3
+* 
+* Note:
+* Bonus points if you could solve it both recursively and iteratively.
+* 
+* confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+* 
+* OJ's Binary Tree Serialization:
+* 
+* The serialization of a binary tree follows a level order traversal, where '#' signifies 
+* a path terminator where no node exists below.
+* 
+* Here's an example:
+* 
+*    1
+*   / \
+*  2   3
+*     /
+*    4
+*     \
+*      5
+* 
+* The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
+* 
+*               
+**********************************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+       if (!root) return true;
+       return isSymmetric(root->left, root->right);
+    }
+    bool isSymmetric(TreeNode* p, TreeNode* q){
+        if (q==NULL && p==NULL) return true;
+        if (q==NULL || p==NULL) return false;
+        return (q->val==p->val && isSymmetric(p->left, q->right) && isSymmetric(p->right, q->left));
+    }
+};
