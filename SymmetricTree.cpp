5de99ac56d8ba1026bf3aabb4c65e72d747859cd
RuneHsu <RuneHsu@users.noreply.github.com>
2016-04-04 23:03:30 +0200

    Update SymmetricTree.cpp

--- SymmetricTree.cpp
+++ SymmetricTree.cpp
@@ -65,3 +65,38 @@ public:
         return (q->val==p->val && isSymmetric(p->left, q->right) && isSymmetric(p->right, q->left));
     }
 };
+
+// Iterative way
+
+class Solution{
+    public:
+    bool isSymmetric(TreeNode* root){
+        if (!root) return true;
+        return isSymmetric(root->left, root->right);
+    }
+    
+    bool isSymmetric(TreeNode *p, TreeNode *q){
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(p);
+        q2.push(q);
+        while (q1.size()>0 && q2.size()>0){
+            TreeNode *p1 = q1.front();
+            q1.pop();
+            TreeNode *p2 = q2.front();
+            q2.pop();
+             
+            if(p1==NULL && p2==NULL) continue;
+            if(p1==NULL || p2==NULL) return false;
+            
+            if(p1->val != p2->val) return false;
+            
+            q1.push(p1->left);                  // it is possible to push a null
+            q2.push(p2->right);
+            
+            q1.push(p1->right);
+            q2.push(p2->left);
+        }
+        return true;
+    }
+};
