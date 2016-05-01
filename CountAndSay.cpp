8941c001da4d2390b7f6393e05fb333fe461b5f0
RuneHsu <RuneHsu@users.noreply.github.com>
2016-05-01 11:09:13 +0200

    Create CountAndSay.cpp

---
+++ CountAndSay.cpp
@@ -0,0 +1,36 @@
+// Source: https://leetcode.com/problems/count-and-say/
+// Date: 2016-05-01
+
+/****************************************************************************
+* The count-and-say sequence is the sequence of integers beginning as follows:
+* 1, 11, 21, 1211, 111221, ...
+*
+* 1 is read off as "one 1" or 11.
+* 11 is read off as "two 1s" or 21.
+* 21 is read off as "one 2, then one 1" or 1211.
+* Given an integer n, generate the nth sequence.
+*
+* Note: The sequence of integers will be represented as a string.
+******************************************************************************/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n==0) return "";
+        string res = "1";
+        
+        while(--n) {
+            string cur;
+            for (int i=0; i<res.size(); i++){
+                int count = 1;
+                while (i+1 < res.size() && res[i]==res[i+1] ){   // calculate the times this number appearance until next diff number
+                    count++;
+                    i++;
+                }
+                cur += to_string(count) + res[i];                   // read as Counted Number
+            }
+            res = cur;               // update the previous digits
+        }
+        return res;
+    }
+};
